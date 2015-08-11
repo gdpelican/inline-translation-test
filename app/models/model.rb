427ce1f5c43c15bc209884e1fc980c5423efb7e0
James Kiesel <james.kiesel@gmail.com>
2015-08-11 13:04:35 -0700

    Add length validations on models

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,6 @@
 class Model < ActiveRecord::Base
   acts_as_translatable on: [:translate_me_body, :translate_me_title]
+
+  validates :translate_me_title, length: { maximum: 50 }
+  validates :translate_me_body, length: { maximum: 100 }
 end
